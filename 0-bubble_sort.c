c88e9f0ce9254410f58a3cd9e3a86feec57fbefa
MikeOdhiambo <odhiambomike413@gmail.com>
2022-07-20 18:57:21 +0300

    Solution 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * bubble_sort - Implement the bubble sort algorithm
+ * @array: Pointer to array of unsorted integers
+ * @size: Size of the array
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t len = size, curr;
+	int temp;
+
+	if (size < 2)
+		return;
+	while (len > 1)
+	{
+		for (curr = 0; curr < len - 1; curr++)
+		{
+			if (array[curr] > array[curr + 1])
+			{
+				temp = array[curr];
+				array[curr] = array[curr + 1];
+				array[curr + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		len--;
+	}
+	return;
+}
